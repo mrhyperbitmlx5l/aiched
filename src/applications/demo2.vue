b419899e2c8be0391c6f3167269babc7abee0049
mrhyperbitmlx5l <baojiao75547@163.com>
2021-09-19 06:47:48 +0800

    20210526  更新Vue 3.0

--- src/applications/demo2.vue
+++ src/applications/demo2.vue
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-<template>
-	<div style="height: 100%;">
-		<a-button type="primary" @click="showModal">模态对话框</a-button>
-		<a-button type="primary" @click="display = true">打开抽屉</a-button>
-		<Dialog v-model="visible" icon="call" title="模态对话框" :inside="true" width="500">
-			<a-form-model layout="inline" :model="formInline"  >
-				<a-form-model-item>
-					<a-input v-model="formInline.user" placeholder="Username"><a-icon slot="prefix" type="user" style="color:rgba(0,0,0,.25)" /></a-input>
-				</a-form-model-item>
-				<a-form-model-item>
-					<a-input v-model="formInline.password" type="password" placeholder="Password"><a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" /></a-input>
-				</a-form-model-item>
-				<a-form-model-item>
-					<a-button type="primary" html-type="submit" :disabled="formInline.user === '' || formInline.password === ''">Log in</a-button>
-				</a-form-model-item>
-			</a-form-model>
-		</Dialog>
-		<drawer title="我是一个抽屉组件" :display.sync="display" :inside="true" :width="drawerWidth" :mask="true">
-			<a-collapse v-model="activeKey">
-				<a-collapse-panel key="1" header="This is panel header 1">
-					<p>{{ text }}</p>
-				</a-collapse-panel>
-				<a-collapse-panel key="2" header="This is panel header 2" :disabled="false">
-					<p>{{ text }}</p>
-				</a-collapse-panel>
-				<a-collapse-panel key="3" header="This is panel header 3" disabled>
-					<p>{{ text }}</p>
-				</a-collapse-panel>
-			</a-collapse>
-		</drawer>
-	</div>
-</template>
-<script>
-import drawer from '@/views/drawer';
-import Dialog from '@/views/DialogModal';
-export default {
-	components: {
-		drawer,
-		Dialog
-	},
-	data() {
-		return {
-			drawerWidth: '500px',
-			display: false,
-			loading: false,
-			visible: false,
-			formInline: {
-				user: '',
-				password: ''
-			},
-			text: `A dog is a type of domesticated animal.Known for its loyalty and faithfulness,it can be found as a welcome guest in many households across the world.`,
-			activeKey: ['1']
-		};
-	},
-	methods: {
-		showModal() {
-			this.visible = true;
-		},
-		handleOk() {
-			this.loading = true;
-			setTimeout(() => {
-				this.visible = false;
-				this.loading = false;
-			}, 3000);
-		},
-		handleCancel() {
-			this.visible = false;
-		},
-		onOk() {
-			alert('close');
-		}
-	}
-};
-</script>
-=======
 <template>
 	<div style="height: 100%;">
 		<a @click="showModal">模态对话框</a>|
@@ -84,7 +8,7 @@ export default {
 </template>
 
 <script>
-import drawer from '@/views/Drawer.vue';
+import drawer from '@/views/drawer.vue';
 import Dialog from '@/views/DialogModal.vue';
 export default {
 	components: {
@@ -127,4 +51,4 @@ export default {
 </script>
 
 <style></style>
->>>>>>> develop
+
--- src/views/Desktop.vue
+++ src/views/Desktop.vue
@@ -2,46 +2,12 @@
 	<div class="desktop">
 		<wallpaper></wallpaper>
 		<icon-grid></icon-grid>
-<<<<<<< HEAD
-		<taskbar></taskbar>
-		<application ></application>
-		<ContextMenu></ContextMenu>
-=======
 		<Taskbar></Taskbar>
 		<ContextMenu></ContextMenu>
 		<Sidebar :sidebarShow="sidebarShow"></Sidebar>
->>>>>>> develop
 	</div>
 </template>
-
 <script>
-<<<<<<< HEAD
-	import Application from './Applications.vue'
-	import Wallpaper from './Wallpaper.vue'
-	import Taskbar from './Taskbar.vue'
-	import IconGrid from './IconGrid.vue'
-	import ContextMenu from './ContextMenu.vue'
-	export default {
-		name: 'Desktop',
-		beforeCreate(){
-			//oncontextmenu 事件在元素中用户右击鼠标时触发并打开上下文菜单
-			document.oncontextmenu = new Function("event.returnValue=false");
-			//onselectstart几乎可以用于所有对象，其触发时间为目标对象被开始选中时（即选中动作刚开始，尚未实质性被选中）
-			//document.onselectstart = new Function("event.returnValue=false");
-			this.$store.commit('manager/init')
-		},
-		components: {
-			Application,
-			Wallpaper,
-			Taskbar,
-			IconGrid,
-			ContextMenu
-		},
-		methods: {
-			clean(){
-				this.$store.commit('manager/selectIcon', '')
-			}
-=======
 import Wallpaper from './Wallpaper.vue'
 import Taskbar from './Taskbar.vue'
 import ContextMenu from './ContextMenu.vue'
@@ -75,7 +41,6 @@ export default {
 	computed: {
 		sidebarShow() {
 			return this.$store.state.core.sidebar;
->>>>>>> develop
 		}
 	}
 };
