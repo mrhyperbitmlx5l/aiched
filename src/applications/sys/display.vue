aed0dfcac2b9d4042303ca43ae98e403bd590a68
mrhyperbitmlx5l <baojiao75547@163.com>
2021-10-03 16:28:48 +0800

    20211027 更新优化 壁纸设置

--- src/applications/sys/display.vue
+++ src/applications/sys/display.vue
@@ -1,55 +1,44 @@
 <template>
 	<div class="display-container">
-		<div class="display-preview"><img :src="preview" /></div>
+		<div class="display-preview"><img :src="data.preview" /></div>
 		<div class="display-list">
-			<div class="display-list-item" :class="{ 'item-selected': item.selected }" v-for="(item, index) in images" :key="index" @click="onSelect(item.url)"><img :src="item.url" /></div>
+			<div class="display-list-item" :class="{ 'item-selected': item.selected }" v-for="(item, index) in data.imglist" :key="index" @click="onSelect(item.url)">
+				<img :src="item.url" />
+			</div>
 		</div>
 	</div>
 </template>
 
-<script>
+<script setup>
 import helper from '@/utils/helper'
-export default {
-	name: 'Display',
-	data() {
-		return {
-			images: [
-				{ url: './wall/wall-0.jpg', selected: false },
-				{ url: './wall/wall-1.jpg', selected: false },
-				{ url: './wall/wall-2.jpg', selected: true },
-				{ url: './wall/wall-3.jpg', selected: false },
-				{ url: './wall/wall-4.jpg', selected: false },
-				{ url: './wall/wall-5.jpg', selected: false }
-			],
-			preview: helper.getLocalstorage('wallpaper',"")
-		};
-	},
-	computed: {
-		wallpaper() {
-			return this.$store.state.core.wallpaper;
+import { reactive } from 'vue'
+import $store from '@/store/index'
+
+let images = [
+	{ url: './wall/wall-0.jpg', selected: false },
+	{ url: './wall/wall-1.jpg', selected: false },
+	{ url: './wall/wall-2.jpg', selected: true },
+	{ url: './wall/wall-3.jpg', selected: false },
+	{ url: './wall/wall-4.jpg', selected: false },
+	{ url: './wall/wall-5.jpg', selected: false }
+];
+
+const data = reactive({ currentIndex: 0, timer: null,preview:helper.getLocalstorage('wallpaper',""),imglist:images });
+
+const selectwallpaper = ()=> {
+	data.imglist.forEach(item => {
+		if(item.url === data.wallpaper){
+			item.selected = true
+		} else {
+			item.selected = false
 		}
-	},
-	created() {
-		this.selectwallpaper()
-	},
-	methods: {
-		selectwallpaper(){
-			this.images.forEach(item => {
-				if(item.url === this.wallpaper){
-					item.selected = true
-				} else {
-					item.selected = false
-				}
-			})
-		},
-		onSelect(url){
-			this.preview = url
-			this.$store.commit('core/SET_WALLPAPER',url);
-			this.selectwallpaper()
-			this.$store.commit('message/PUSH_MESSAGE', {'type':'notification','content':'切换成功1111111111111111111111111111111111111111111111111111111111111111','title':'更换壁纸'})
-		}
-	}
-};
+	})
+}
+const onSelect = (url) =>{
+	data.preview = url
+	$store.commit('core/SET_WALLPAPER',url);
+	selectwallpaper()
+}
 </script>
 <style scoped>
 .display-container {
@@ -59,21 +48,42 @@ export default {
 	margin: 0;
 }
 .display-preview {
-	width: 60%;
+	width: 100%;height: 100%;
 	margin: 0px auto;
-	padding: 15px;
 }
 .display-preview img {
-	width: 100%;
+	width: 100%;object-fit: cover;
 }
 .display-list {
-	width: 100%;
+	width: 80%;
 	white-space: nowrap;
+	position: absolute;
+	bottom: 10%;
+	left:50%;
+	-ms-transform:translate(-50%,0%); /* IE 9 */
+	-webkit-transform:translate(-50%,0%); /* Safari and Chrome */
+	transform: translate(-50%,0%);
+	background-color: rgba(0, 0, 0, 0.35);
+	box-shadow: 0 10px 25px rgba(0, 0, 0, 0.6);
+	border-radius: 5px;
+	padding: 10px 10px;
 	overflow-x: auto;
-	-webkit-overflow-scrolling: touch;
-	margin: 0px auto;
-	padding: 10px 20px;
 }
+
+.display-list::-webkit-scrollbar {
+		width: 3px;
+		height: 5px;
+}
+.display-list::-webkit-scrollbar-thumb {
+	border-radius: 5px;
+	background: #5555ff;
+}
+.display-list::-webkit-scrollbar-track {
+	-webkit-box-shadow: inset 0 0 1px rgba(0, 0, 0, 0);
+	border-radius: 3px;
+	background: #ffffff;
+}
+
 .display-list-item {
 	margin: 5px;
 	display: inline-block;
@@ -89,6 +99,6 @@ export default {
 }
 
 .display-list-item img {
-	height: 130px;
+	height: 120px;
 }
 </style>
